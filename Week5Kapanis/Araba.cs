b3436129b9c9f11dbda7e114ced21017251e43d7
auro <auro@DESKTOP-AV8I9BT>
2024-08-26 00:44:29 +0300

    Add project files.

---
+++ Week5Kapanis/Araba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Kapanis
+{
+    internal class Araba
+    {
+        public DateTime UretimTarihi { get; set; }
+        public int SeriNumarasi { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+        public int KapiSayisi { get; set; }
+
+
+
+
+
+    }
+}
---
+++ Week5Kapanis/Program.cs
@@ -0,0 +1,56 @@
+﻿using Week5Kapanis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Araba> yeniAraba = new List<Araba>();
+
+        while (true)
+        {
+            Console.WriteLine("Bir araba üretmek için 'e' yoksa 'h' tuşuna basınız.");
+            string secim = Console.ReadLine().ToLower();
+            if (secim == "h")
+            {
+                Console.WriteLine("İyi günler dileriz.");
+                break;
+            }
+            else if (secim == "e")
+            {
+                Araba araba = new Araba();
+                Console.WriteLine("Lütfen arabanın seri numarasını giriniz.");
+                araba.SeriNumarasi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Lütfen arabanın markasını giriniz.");
+                araba.Marka = Console.ReadLine();
+                Console.WriteLine("Lütfen arabanın modelini giriniz.");
+                araba.Model = Console.ReadLine();
+                Console.WriteLine("Lütfen arabanın rengini giriniz.");
+                araba.Renk = Console.ReadLine();
+            kapiGirisi: Console.WriteLine("Lütfen arabanın kapı sayısını giriniz.");
+                string kapiSayisi = Console.ReadLine();
+                try
+                {
+                    araba.KapiSayisi = int.Parse(kapiSayisi);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Geçersiz bir format girişi yaptınız.");
+                    goto kapiGirisi;
+                }
+                yeniAraba.Add(araba);
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz bir giriş yaptınız.");
+            }
+
+            Console.WriteLine("\n*** ÜRETİLEN ARABALAR***");
+            foreach (var araba in yeniAraba)
+            {
+                Console.WriteLine($"Arabanın Seri Numarası : {araba.SeriNumarasi}" +
+                                  $"\nArabanın Markası : {araba.Marka}");
+
+            }
+        }
+    }
+}
